daa22fc782dc901919cf1e93dbb4c22c4031a8b5
khsaad04 <khsaad0404@gmail.com>
2024-04-15 13:17:20 +0600

    Add flake-parts to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,46 +1,45 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          clang-tools
-          nil
-          gdb
-          astyle
-          raylib
-        ];
-      };
-      packages.${system} = {
-        breakout = pkgs.stdenv.mkDerivation {
-          name = "breakout";
-          src = ./.;
-          buildInputs = [ pkgs.raylib ];
-          buildPhase = ''
-            mkdir -p $out/bin
-            cc -Wall -Wextra -o breakout breakout.c -lraylib
-            chmod +x breakout
-            cp breakout $out/bin
-          '';
+  outputs = { flake-parts, ... } @ inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      perSystem = { pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            clang-tools
+            nil
+            gdb
+            raylib
+          ];
         };
-        flappy-bird = pkgs.stdenv.mkDerivation {
-          name = "flappy_bird";
-          src = ./.;
-          buildInputs = [ pkgs.raylib ];
-          buildPhase = ''
-            mkdir -p $out/bin
-            cc -Wall -Wextra -o flappy_bird flappy_bird.c -lraylib
-            chmod +x flappy_bird
-            cp flappy_bird $out/bin
-          '';
+        packages = {
+          breakout = pkgs.stdenv.mkDerivation {
+            name = "breakout";
+            src = ./.;
+            buildInputs = [ pkgs.raylib ];
+            buildPhase = ''
+              mkdir -p $out/bin
+              cc -Wall -Wextra -o breakout breakout.c -lraylib
+              chmod +x breakout
+              cp breakout $out/bin
+            '';
+          };
+          flappy-bird = pkgs.stdenv.mkDerivation {
+            name = "flappy_bird";
+            src = ./.;
+            buildInputs = [ pkgs.raylib ];
+            buildPhase = ''
+              mkdir -p $out/bin
+              cc -Wall -Wextra -o flappy_bird flappy_bird.c -lraylib
+              chmod +x flappy_bird
+              cp flappy_bird $out/bin
+            '';
+          };
         };
+        formatter = pkgs.nixpkgs-fmt;
       };
-      formatter.${system} = pkgs.nixpkgs-fmt;
     };
 }
