f6fa240ae333db157f4f837b01f3ef2df760ff5e
khsaad04 <khsaad0404@gmail.com>
2024-04-18 13:06:23 +0600

    Use clang instead of gcc

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ if [ ! -d "build" ]; then
     mkdir build
 fi
 
-cc -Wall -Wextra -o build/breakout breakout.c -lraylib
-cc -Wall -Wextra -o build/flappy_bird flappy_bird.c -lraylib
+clang -O3 -Wall -Wextra -o build/breakout breakout.c -lraylib
+clang -O3 -Wall -Wextra -o build/flappy_bird flappy_bird.c -lraylib
