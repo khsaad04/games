d49c6c66ad3164650ac9bbb737aa234268491a5e
khsaad04 <khsaad0404@gmail.com>
2024-04-19 12:13:35 +0600

    Implement scoring and bird death effect

--- flappy_bird.c
+++ flappy_bird.c
@@ -1,4 +1,5 @@
 #include <raylib.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
 static const int screen_width = 800;
@@ -18,7 +19,7 @@ typedef struct {
 
 typedef struct {
     Vector2 pos;
-    Color color;
+    bool passed;
 } Pillar;
 
 static enum State game;
@@ -52,7 +53,7 @@ void init_game(void)
     for (int i = 0; i < 3; ++i) {
         pillars[i].pos =
             (Vector2){screen_width + i * pillar_padding, random_piller_y()};
-        pillars[i].color = RAYWHITE;
+        pillars[i].passed = false;
     }
 }
 
@@ -73,21 +74,28 @@ void update_game(void)
             game = PAUSED;
         }
 
-        for (int i = 0; i < 3; ++i) {
-            pillars[i].pos.x -= pillar_velocity * GetFrameTime();
-            if (pillars[i].pos.x < -pillar_width) {
-                pillars[i].pos.x = screen_width;
-                pillars[i].pos.y = random_piller_y();
-            }
-        }
-
-        // Collision
         if (bird.pos.y < 0 - bird_radius ||
             bird.pos.y > screen_height + bird_radius) {
             game = OVER;
+            bird.color = RED;
         }
 
         for (int i = 0; i < 3; ++i) {
+            pillars[i].pos.x -= pillar_velocity * GetFrameTime();
+
+            if (pillars[i].pos.x < -pillar_width) {
+                pillars[i].pos.x = screen_width;
+                pillars[i].pos.y = random_piller_y();
+                pillars[i].passed = false;
+            }
+
+            if (pillars[i].pos.x <= bird.pos.x) {
+                if (!pillars[i].passed) {
+                    score += 1;
+                }
+                pillars[i].passed = true;
+            }
+
             Rectangle pillar_top_rect = {
                 .x = pillars[i].pos.x,
                 .y = 0,
@@ -101,11 +109,13 @@ void update_game(void)
                 .width = pillar_width,
                 .height = screen_height,
             };
+
             if (CheckCollisionCircleRec(bird.pos, bird_radius,
                                         pillar_top_rect) ||
                 CheckCollisionCircleRec(bird.pos, bird_radius,
                                         pillar_bottom_rect)) {
                 game = OVER;
+                bird.color = RED;
             }
         }
     }
@@ -128,14 +138,24 @@ void draw_game(void)
 {
     BeginDrawing();
     ClearBackground(BLACK);
+
+    // Score
+    DrawText(TextFormat("%d", score),
+             screen_width / 2 - MeasureText(TextFormat("%d", score), 50) / 2,
+             screen_height / 2, 50, GRAY);
+
+    // Bird
     DrawEllipse(bird.pos.x, bird.pos.y, bird.radius.x, bird.radius.y,
                 bird.color);
+
+    // Pillars
     for (int i = 0; i < 3; ++i) {
         DrawRectangle(pillars[i].pos.x, 0, pillar_width, pillars[i].pos.y,
-                      pillars[i].color);
+                      RAYWHITE);
         DrawRectangle(pillars[i].pos.x, pillars[i].pos.y + pillar_gap,
-                      pillar_width, screen_height, pillars[i].color);
+                      pillar_width, screen_height, RAYWHITE);
     }
+
     EndDrawing();
 }
 
