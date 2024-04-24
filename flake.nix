cace91164276ffe9dfb18b3cce8f0e3c937f86a3
khsaad04 <khsaad0404@gmail.com>
2024-04-24 16:40:34 +0600

    Update nix flake

--- flake.nix
+++ flake.nix
@@ -3,40 +3,46 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = { flake-parts, ... } @ inputs:
+  outputs =
+    { flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" ];
-      perSystem = { pkgs, ... }: {
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.clang-tools
-            pkgs.clang
-            pkgs.nil
-            pkgs.gdb
-            pkgs.raylib
-          ];
-        };
-        packages =
-          let
-            buildPackage = name: pkgs.stdenv.mkDerivation {
-              name = "${name}";
-              src = ./.;
-              nativeBuildInputs = [ pkgs.clang ];
-              buildInputs = [ pkgs.raylib pkgs.pkg-config ];
-              buildPhase = ''
-                mkdir -p $out/bin
-                clang -O3 -Wall -Wextra -o ${name} ${name}.c -lraylib
-                chmod +x ${name}
-                cp ${name} $out/bin
-              '';
-            };
-          in
-          rec{
-            default = breakout;
-            breakout = buildPackage "breakout";
-            flappy_bird = buildPackage "flappy_bird";
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.clang-tools
+              pkgs.clang
+              pkgs.gdb
+              pkgs.raylib
+            ];
           };
-        formatter = pkgs.nixpkgs-fmt;
-      };
+          packages =
+            let
+              buildPackage =
+                name:
+                pkgs.stdenv.mkDerivation {
+                  name = "${name}";
+                  src = ./.;
+                  nativeBuildInputs = [ pkgs.clang ];
+                  buildInputs = [
+                    pkgs.raylib
+                    pkgs.pkg-config
+                  ];
+                  buildPhase = ''
+                    mkdir -p $out/bin
+                    clang -O3 -Wall -Wextra -o ${name} ${name}.c -lraylib
+                    chmod +x ${name}
+                    cp ${name} $out/bin
+                  '';
+                };
+            in
+            rec {
+              default = breakout;
+              breakout = buildPackage "breakout";
+              flappy_bird = buildPackage "flappy_bird";
+            };
+        };
     };
 }
