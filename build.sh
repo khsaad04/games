562d9ad34802e47d74794107e741d748e9836caf
khsaad04 <khsaad0404@gmail.com>
2024-04-28 23:11:47 +0600

    Init snake game

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ if [ ! -d "build" ]; then
 fi
 
 clang -O3 -Wall -Wextra -o build/breakout src/breakout.c -lraylib
-clang -O3 -Wall -Wextra -o build/flappy_bird src/alappy_bird.c -lraylib
+clang -O3 -Wall -Wextra -o build/flappy_bird src/flappy_bird.c -lraylib
+clang -O3 -Wall -Wextra -o build/snake src/snake.c -lraylib
