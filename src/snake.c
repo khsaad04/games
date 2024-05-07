60ceaf267c58627f847b132135e3201376547962
khsaad04 <khsaad0404@gmail.com>
2024-05-07 12:13:16 +0600

    snake: Use switch cases to handle game state

--- src/snake.c
+++ src/snake.c
@@ -1,11 +1,20 @@
+#include <assert.h>
 #include <raylib.h>
 #include <stdbool.h>
 #include <stdlib.h>
 
+#define SNAKE_COLOR BLUE
+#define APPLE_COLOR RED
+#define BG1                                                                    \
+    (Color) { 20, 20, 20, 255 }
+#define BG2                                                                    \
+    (Color) { 10, 10, 10, 255 }
+
 static const int screen_width = 800;
 static const int screen_height = 600;
-static const int fps = 24;
-static const int cell_size = 31;
+static const int fps = 12;
+static const int cell_size =
+    33; // Must be such value that rows and cols are even numbered
 static const Vector2 offset =
     (Vector2){screen_width % cell_size, screen_height % cell_size};
 static const int rows = (screen_height - offset.y) / cell_size;
@@ -16,13 +25,11 @@ enum State { STANDBY, RUNNING, PAUSED, OVER };
 
 typedef struct {
     Vector2 pos;
-    Vector2 size;
     Vector2 speed;
 } SnakeCell;
 
 typedef struct {
     Vector2 pos;
-    Vector2 size;
     bool active;
 } Apple;
 
@@ -42,54 +49,65 @@ void init_game(void)
     game = STANDBY;
     score = 0;
     snake.len = 1;
+    assert(rows % 2 == 0);
+    assert(cols % 2 == 0);
     for (size_t i = 0; i < snake_cap; ++i) {
-        snake.cells[i].pos = (Vector2){offset.x / 2, offset.y / 2};
-        snake.cells[i].size = (Vector2){cell_size, cell_size};
+        snake.cells[i].pos =
+            (Vector2){offset.x / 2 + (cols / 2.0 - 3) * cell_size,
+                      offset.y / 2 + (rows / 2.0) * cell_size};
         snake.cells[i].speed = (Vector2){cell_size, 0};
     }
 
-    apple.size = (Vector2){cell_size, cell_size};
     apple.active = false;
+}
+
+void controls_update(void)
+{
+    if (IsKeyPressed(KEY_P) && game != OVER) {
+        game = PAUSED;
+    }
+    if (IsKeyPressed(KEY_RIGHT) && (snake.cells[0].speed.x == 0)) {
+        snake.cells[0].speed = (Vector2){cell_size, 0};
+    }
+    if (IsKeyPressed(KEY_LEFT) && (snake.cells[0].speed.x == 0)) {
+        snake.cells[0].speed = (Vector2){-cell_size, 0};
+    }
+    if (IsKeyPressed(KEY_UP) && (snake.cells[0].speed.y == 0)) {
+        snake.cells[0].speed = (Vector2){0, -cell_size};
+    }
+    if (IsKeyPressed(KEY_DOWN) && (snake.cells[0].speed.y == 0)) {
+        snake.cells[0].speed = (Vector2){0, cell_size};
+    }
 }
 
 void update_game(void)
 {
-    if (game == RUNNING) {
+    switch (game) {
+    case STANDBY:
+        if (IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_UP) ||
+            IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_RIGHT)) {
+            controls_update();
+            game = RUNNING;
+        }
+        break;
+    case RUNNING:
         for (size_t i = 0; i < snake.len; ++i) {
             snake_pos[i] = snake.cells[i].pos;
         }
 
-        for (size_t i = 0; i < snake_cap; ++i) {
-            if (i == 0) {
-                snake.cells[0].pos.x += snake.cells[0].speed.x;
-                snake.cells[0].pos.y += snake.cells[0].speed.y;
-            }
-            else {
-                snake.cells[i].pos = snake_pos[i - 1];
-            }
+        snake.cells[0].pos.x += snake.cells[0].speed.x;
+        snake.cells[0].pos.y += snake.cells[0].speed.y;
+        for (size_t i = 1; i < snake_cap; ++i) {
+            snake.cells[i].pos = snake_pos[i - 1];
         }
 
-        for (size_t i = 0; i < snake.len; ++i) {
-            if (snake.cells[i].pos.x < offset.x / 2 ||
-                snake.cells[i].pos.x >
-                    screen_width - offset.x / 2 - cell_size ||
-                snake.cells[i].pos.y < offset.y / 2 ||
-                snake.cells[i].pos.y > screen_height - offset.x / 2 - cell_size)
-                game = OVER;
-        }
+        if (snake.cells[0].pos.x < offset.x / 2 ||
+            snake.cells[0].pos.x + cell_size > screen_width - offset.x / 2 ||
+            snake.cells[0].pos.y < offset.y / 2 ||
+            snake.cells[0].pos.y + cell_size > screen_height - offset.x / 2)
+            game = OVER;
 
-        if (IsKeyPressed(KEY_RIGHT) && (snake.cells[0].speed.x == 0)) {
-            snake.cells[0].speed = (Vector2){cell_size, 0};
-        }
-        if (IsKeyPressed(KEY_LEFT) && (snake.cells[0].speed.x == 0)) {
-            snake.cells[0].speed = (Vector2){-cell_size, 0};
-        }
-        if (IsKeyPressed(KEY_UP) && (snake.cells[0].speed.y == 0)) {
-            snake.cells[0].speed = (Vector2){0, -cell_size};
-        }
-        if (IsKeyPressed(KEY_DOWN) && (snake.cells[0].speed.y == 0)) {
-            snake.cells[0].speed = (Vector2){0, cell_size};
-        }
+        controls_update();
 
         if (!apple.active) {
             apple.active = true;
@@ -99,7 +117,7 @@ void update_game(void)
                 GetRandomValue(0, (screen_height / cell_size) - 1) * cell_size +
                     offset.y / 2};
 
-            for (int i = 0; i < snake.len; i++) {
+            for (size_t i = 0; i < snake.len; i++) {
                 while ((apple.pos.x == snake.cells[i].pos.x) &&
                        (apple.pos.y == snake.cells[i].pos.y)) {
                     apple.pos = (Vector2){
@@ -114,12 +132,13 @@ void update_game(void)
             }
         }
 
-        if ((snake.cells[0].pos.x < (apple.pos.x + apple.size.x) &&
-             (snake.cells[0].pos.x + snake.cells[0].size.x) > apple.pos.x) &&
-            (snake.cells[0].pos.y < (apple.pos.y + apple.size.y) &&
-             (snake.cells[0].pos.y + snake.cells[0].size.y) > apple.pos.y)) {
+        if ((snake.cells[0].pos.x < (apple.pos.x + cell_size) &&
+             (snake.cells[0].pos.x + cell_size) > apple.pos.x) &&
+            (snake.cells[0].pos.y < (apple.pos.y + cell_size) &&
+             (snake.cells[0].pos.y + cell_size) > apple.pos.y)) {
             snake.cells[snake.len].pos = snake_pos[snake.len - 1];
             snake.len += 1;
+            score += 1;
             apple.active = false;
         }
 
@@ -129,51 +148,66 @@ void update_game(void)
                 game = OVER;
             }
         }
-    }
-    else if (game == PAUSED && IsKeyPressed(KEY_P)) {
-        game = RUNNING;
-    }
-
-    else if (game == OVER &&
-             (IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_UP) ||
-              IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_RIGHT))) {
-        init_game();
-        game = STANDBY;
-    }
-
-    else if (game != OVER &&
-             (IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_UP) ||
-              IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_RIGHT))) {
-        game = RUNNING;
+        break;
+    case PAUSED:
+        if (IsKeyPressed(KEY_P)) {
+            game = RUNNING;
+        }
+        break;
+    case OVER:
+        if (IsKeyPressed(KEY_DOWN) || IsKeyPressed(KEY_UP) ||
+            IsKeyPressed(KEY_LEFT) || IsKeyPressed(KEY_RIGHT)) {
+            init_game();
+            game = STANDBY;
+        }
+        break;
     }
 }
 
 void draw_game(void)
 {
     BeginDrawing();
-    ClearBackground(BLACK);
+    ClearBackground(BG1);
     for (size_t i = 0; i < rows; ++i) {
         for (size_t j = 0; j < cols; ++j) {
             if ((i + j) % 2 == 0) {
                 DrawRectangleV((Vector2){offset.x / 2 + j * cell_size,
                                          offset.y / 2 + i * cell_size},
-                               (Vector2){cell_size, cell_size},
-                               GetColor(0x252525));
+                               (Vector2){cell_size, cell_size}, BG2);
             }
         }
     }
+    DrawLineV((Vector2){offset.x / 2, offset.y / 2},
+              (Vector2){screen_width - offset.x / 2, offset.y / 2}, BG2);
+
+    DrawLineV(
+        (Vector2){offset.x / 2, screen_height - offset.y / 2},
+        (Vector2){screen_width - offset.x / 2, screen_height - offset.y / 2},
+        BG2);
+
+    DrawLineV((Vector2){offset.x / 2, offset.y / 2},
+              (Vector2){offset.x / 2, screen_height - offset.y / 2}, BG2);
+
+    DrawLineV(
+        (Vector2){screen_width - offset.x / 2, offset.y / 2},
+        (Vector2){screen_width - offset.x / 2, screen_height - offset.y / 2},
+        BG2);
 
     for (size_t i = 0; i < snake.len; ++i) {
-        DrawRectangleV(snake.cells[i].pos, snake.cells[i].size, BLUE);
+        DrawRectangleV(snake.cells[i].pos, (Vector2){cell_size, cell_size},
+                       SNAKE_COLOR);
     }
     if (apple.active)
-        DrawRectangleV(apple.pos, apple.size, RED);
+        DrawRectangleV(apple.pos, (Vector2){cell_size, cell_size}, APPLE_COLOR);
+
+    DrawText(TextFormat("Score: %d", score), 20, screen_height - 25, 20,
+             RAYWHITE);
     EndDrawing();
 }
 
 int main(void)
 {
-    InitWindow(screen_width, screen_height, "Flappy Bird");
+    InitWindow(screen_width, screen_height, "Snake");
     SetTargetFPS(fps);
 
     init_game();
