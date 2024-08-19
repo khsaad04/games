1e50fed23049e9a5025ed53d7c36dd6962924ea4
khsaad04 <khsaad0404@gmail.com>
2024-08-19 12:11:51 +0600

    fix: build script uses cc instead of clang

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ if [ ! -d "build" ]; then
     mkdir build
 fi
 
-clang -Wall -Wextra -o build/breakout src/breakout.c -lraylib -ggdb
-clang -Wall -Wextra -o build/flappy_bird src/flappy_bird.c -lraylib -ggdb
-clang -Wall -Wextra -o build/snake src/snake.c -lraylib -ggdb
+cc -Wall -Wextra -o build/breakout src/breakout.c -lraylib -ggdb
+cc -Wall -Wextra -o build/flappy_bird src/flappy_bird.c -lraylib -ggdb
+cc -Wall -Wextra -o build/snake src/snake.c -lraylib -ggdb
