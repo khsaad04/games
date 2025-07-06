2bf5bb07c2c633f83e7d3e42381de9f93836a7b0
khsaad04 <khsaad0404@gmail.com>
2025-07-06 11:13:09 +0600

    Go back to synchronous building

--- nob.c
+++ nob.c
@@ -6,10 +6,10 @@
 #define SRC_FOLDER "src/"
 #define RAYLIB_FOLDER "raylib-5.5_linux_amd64/"
 
-void build_game(Cmd *cmd, Procs *procs, char *name)
+void build_game(Cmd *cmd, char *name)
 {
-    cmd_append(
-        cmd, "cc",
+    cmd_append(cmd,
+        "cc",
         "-Wall", "-Wextra", "-ggdb", "-O3",
         "-I./"RAYLIB_FOLDER"include",
         "-o", temp_sprintf("%s%s", BUILD_FOLDER, name),
@@ -17,7 +17,7 @@ void build_game(Cmd *cmd, Procs *procs, char *name)
         "-L./"RAYLIB_FOLDER"lib", "-l:libraylib.a", "-lm"
     );
 
-    da_append(procs, cmd_run_async_and_reset(cmd));
+    cmd_run_sync_and_reset(cmd);
 }
 
 int main(int argc, char **argv)
@@ -27,13 +27,10 @@ int main(int argc, char **argv)
     if (!mkdir_if_not_exists(BUILD_FOLDER)) return 1;
 
     Cmd cmd = {0};
-    Procs procs = {0};
 
-    build_game(&cmd, &procs, "breakout");
-    build_game(&cmd, &procs, "flappy_bird");
-    build_game(&cmd, &procs, "snake");
-
-    if (!procs_wait_and_reset(&procs)) return 1;
+    build_game(&cmd, "breakout");
+    build_game(&cmd, "flappy_bird");
+    build_game(&cmd, "snake");
 
     return 0;
 }
