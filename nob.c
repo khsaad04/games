e0b720e9ddcb6bd2b46934df41773329bff0cde0
khsaad04 <khsaad0404@gmail.com>
2025-07-12 12:11:23 +0600

    [nob] check for failure in running cmd

--- nob.c
+++ nob.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #define NOB_IMPLEMENTATION
 #define NOB_STRIP_PREFIX
 #include "nob.h"
@@ -6,7 +7,7 @@
 #define SRC_FOLDER "src/"
 #define RAYLIB_FOLDER "raylib-5.5_linux_amd64/"
 
-void build_game(Cmd *cmd, char *name)
+bool build_game(Cmd *cmd, char *name)
 {
     cmd_append(cmd,
         "cc",
@@ -17,7 +18,8 @@ void build_game(Cmd *cmd, char *name)
         "-L./"RAYLIB_FOLDER"lib", "-l:libraylib.a", "-lm"
     );
 
-    cmd_run_sync_and_reset(cmd);
+    if (!cmd_run_sync_and_reset(cmd)) return false;
+    return true;
 }
 
 int main(int argc, char **argv)
@@ -28,9 +30,9 @@ int main(int argc, char **argv)
 
     Cmd cmd = {0};
 
-    build_game(&cmd, "breakout");
-    build_game(&cmd, "flappy_bird");
-    build_game(&cmd, "snake");
+    if (!build_game(&cmd, "breakout")) return 1;
+    if (!build_game(&cmd, "flappy_bird")) return 1;
+    if (!build_game(&cmd, "snake")) return 1;
 
     return 0;
 }
